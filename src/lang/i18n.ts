b09afbf628aa670c77c7841972854afb4a6f69ca
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-10-24 19:04:34 -0300

    feat: add basic pt translation

--- src/lang/i18n.ts
+++ src/lang/i18n.ts
@@ -2,14 +2,16 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 import en from "./locales/en.json";
+import pt from "./locales/pt.json";
 
 const resources = {
-  en
+  en,
+  pt
 };
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: "en"
+  lng: "pt"
 });
 
 export default { i18n };
