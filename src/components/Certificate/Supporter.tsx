9ef64f63db5e83709f4d0fe2682911569ad00b5e
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-11-04 19:17:19 -0300

    feat: add share supporter certificate

--- src/components/Certificate/Supporter.tsx
+++ src/components/Certificate/Supporter.tsx
@@ -7,6 +7,7 @@ import { Icon, Text } from "@components";
 
 //@ts-ignore
 import RCLogo from "../../assets/images/rc.png";
+import { DownloadContainer } from "./DownloadContainer";
 
 interface Props {
   address: string;
@@ -27,59 +28,61 @@ export function Supporter({ address }: Props) {
   const qrLink = 'https://users.regenerationcredit.org/supporter/' + address + '/' + parsedName;
 
   return (
-    <View className="w-full max-w-[500] border-2 border-white rounded-2xl p-3 gap-5">
-      <View className="flex-row items-center justify-center gap-3 border-b border-card-secondary pb-5 pt-3">
-        <Icon name="rc" size={30} />
-        <Text className="text-white font-semibold">{t('common.regenerationCredit')}</Text>
-      </View>
-      <View className="flex-row justify-between">
-        <View className="w-[48%]">
-          <Text className="text-white font-semibold" numberOfLines={1}>{supporter?.name}</Text>
-          <View className="flex-row items-center flex-wrap">
-            <Text className="text-white">{t('profile.contributedWith')}</Text>
-            <Text className="font-bold text-green-500 mx-1">{Intl.NumberFormat('pt-BR').format(certificated)}</Text>
-            <Text className="text-white">RC</Text>
+    <DownloadContainer>
+      <View className="w-full max-w-[500] border-2 border-white rounded-2xl p-3 gap-5 bg-card-primary">
+        <View className="flex-row items-center justify-center gap-3 border-b border-card-secondary pb-5 pt-3">
+          <Icon name="rc" size={30} />
+          <Text className="text-white font-semibold">{t('common.regenerationCredit')}</Text>
+        </View>
+        <View className="flex-row justify-between">
+          <View className="w-[48%]">
+            <Text className="text-white font-semibold" numberOfLines={1}>{supporter?.name}</Text>
+            <View className="flex-row items-center flex-wrap">
+              <Text className="text-white">{t('profile.contributedWith')}</Text>
+              <Text className="font-bold text-green-500 mx-1">{Intl.NumberFormat('pt-BR').format(certificated)}</Text>
+              <Text className="text-white">RC</Text>
+            </View>
+
+            <Text className="text-gray-300 text-xs mt-3">{t('common.impact')}</Text>
+            <View className="flex-row items-center gap-1">
+              <Text className="text-white text-sm">{t('common.carbon')}:</Text>
+              <Text className="text-white text-sm font-semibold">
+                {Intl.NumberFormat('pt-BR', {maximumFractionDigits: 7}).format(carbonImpact)} g
+              </Text>
+            </View>
+            <View className="flex-row items-center gap-1">
+              <Text className="text-white text-sm">{t('common.trees')}:</Text>
+              <Text className="text-white text-sm font-semibold">
+                {Intl.NumberFormat('pt-BR', {maximumFractionDigits: 7}).format(treesImpact)}
+              </Text>
+            </View>
+            <View className="flex-row items-center gap-1">
+              <Text className="text-white text-sm">{t('common.biodiversity')}:</Text>
+              <Text className="text-white text-sm font-semibold">
+                {Intl.NumberFormat('pt-BR', {maximumFractionDigits: 7}).format(bioImpact)}
+              </Text>
+            </View>
+            <View className="flex-row items-center gap-1">
+              <Text className="text-white text-sm">{t('common.area')}:</Text>
+              <Text className="text-white text-sm font-semibold">
+                {Intl.NumberFormat('pt-BR', {maximumFractionDigits: 5}).format(areaImpact)} m²
+              </Text>
+            </View>
           </View>
 
-          <Text className="text-gray-300 text-xs mt-3">{t('common.impact')}</Text>
-          <View className="flex-row items-center gap-1">
-            <Text className="text-white text-sm">{t('common.carbon')}:</Text>
-            <Text className="text-white text-sm font-semibold">
-              {Intl.NumberFormat('pt-BR', {maximumFractionDigits: 7}).format(carbonImpact)} g
-            </Text>
-          </View>
-          <View className="flex-row items-center gap-1">
-            <Text className="text-white text-sm">{t('common.trees')}:</Text>
-            <Text className="text-white text-sm font-semibold">
-              {Intl.NumberFormat('pt-BR', {maximumFractionDigits: 7}).format(treesImpact)}
-            </Text>
-          </View>
-          <View className="flex-row items-center gap-1">
-            <Text className="text-white text-sm">{t('common.biodiversity')}:</Text>
-            <Text className="text-white text-sm font-semibold">
-              {Intl.NumberFormat('pt-BR', {maximumFractionDigits: 7}).format(bioImpact)}
-            </Text>
-          </View>
-          <View className="flex-row items-center gap-1">
-            <Text className="text-white text-sm">{t('common.area')}:</Text>
-            <Text className="text-white text-sm font-semibold">
-              {Intl.NumberFormat('pt-BR', {maximumFractionDigits: 5}).format(areaImpact)} m²
-            </Text>
+          <View className="w-[48%] items-center justify-center">
+            <QRCode
+              value={qrLink}
+              size={130}
+              logo={RCLogo}
+              logoSize={30}
+              logoBorderRadius={30}
+            />
           </View>
         </View>
 
-        <View className="w-[48%] items-center justify-center">
-          <QRCode
-            value={qrLink}
-            size={130}
-            logo={RCLogo}
-            logoSize={30}
-            logoBorderRadius={30}
-          />
-        </View>
+        <Text className="text-white text-sm text-center my-1">{address}</Text>
       </View>
-
-      <Text className="text-white text-sm text-center my-1">{address}</Text>
-    </View>
+    </DownloadContainer>
   )
 }
