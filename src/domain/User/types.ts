5b0a33a96ccc381f6bc07897234aad43b8d4c3f4
rav-andre <aoravagnani@gmail.com>
2026-04-28 22:26:09 -0300

    user level

--- src/domain/User/types.ts
+++ src/domain/User/types.ts
@@ -3,5 +3,7 @@ export interface BasicUserProps {
   name: string;
   photo: string;
   poolLevel: number;
+  extraInfo?: string;
+  extraInfoValue?: number;
 }
 export type UserType = 1 | 2 | 3 | 4 | 5 | 6 | 7
